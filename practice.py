259c096327d7470a47a77c91404c50593c0bd847
Anthony Chi <a35931chi@gmail.com>
2018-05-01 00:24:33 -0400

    start testing if data pulling function works

--- practice.py
+++ practice.py
@@ -275,7 +275,7 @@ def zri(df_state_city_zip_combined):
             print(e)
 
     print('from Zillow API:')
-    print('original shape: {}'.format(zri.shape)) #shape as of 2018-04-25 ()
+    print('original shape: {}'.format(zri.shape)) #shape as of 2018-04-25 (11137, 92)
     #print(zhvi.duplicated(keep = 'first').sum()) #
     #print(zhvi.duplicated(keep = 'first').sum())
 
@@ -285,12 +285,12 @@ def zri(df_state_city_zip_combined):
     #we need to drop some columns and rename some columns
     zri_T.rename(columns = {'Data Type': 'Home Type', 'Region Name': 'zip'}, inplace = True)
     zri_T.drop(['Region Type'], inplace = True, axis = 1)
-    print(zri_T.shape) #(632589, 4)
+    print(zri_T.shape) #(954433, 4)
     zri_T.drop_duplicates(keep = 'first', inplace = True) #
-    print(zri_T.shape) #(1103534, 3)
+    print(zri_T.shape) #(224976, 4)
     
 
-     #Zillow Rental Index -
+    #Zillow Rental Index -
     quandl_zri = {'ZRIAHMF': 'All Homes Multi Fam',
                   'ZRIAH': 'All Homes',
                   'ZRIMFRR': 'Multi Fam',
@@ -334,172 +334,6 @@ def zri(df_state_city_zip_combined):
     df_quandl_zri.reset_index(inplace = True)
 
     return zri_T, df_quandl_zri
-
-
-def zri_reference(df_state_city_zip_combined):
-    #zillow rental index portion
-    zri = pd.DataFrame()
-
-    for link in df_state_city_zip_combined['zri_url']:
-        print(link)
-        try:
-            xls = pd.ExcelFile(link)
-            for i in range(len(xls.sheet_names)):
-                temp = xls.parse(i, header=1)
-                temp.columns.values[3:] = [datetime.datetime(i.year, i.month, 1) for i in temp.columns.values[3:]]
-                zri = pd.concat([zri, temp], axis = 0) 
-        except Exception as e:
-            print(e)
-
-    print(zri.shape) #(60850, 84)
-    print(zri.duplicated(keep = 'first').sum()) #57887
-    zri.drop_duplicates(keep = 'first', inplace = True)
-    print(zri.shape) #(2963, 84)
-    print(zri.duplicated(keep = 'first').sum())
-
-    zri_T = zri.set_index(['Data Type','Region Name','Region Type']).stack().reset_index().rename(columns = {'level_3': 'Date', 0: 'zri'})
-    zri_T['Region Name'] = zri_T['Region Name'].apply(lambda x: str(x).zfill(5))
-    zri_T.rename(columns = {'Data Type':'Home Type', 'Region Name': 'zip'}, inplace = True)
-    zri_T.drop(['Region Type'], inplace = True, axis = 1)
-    zri_T['d'] = 2
-
-    #standard code to pull my data
-    engine = create_engine('mysql+pymysql://a35931chi:Maggieyi66@localhost/realestate')
-
-    #zillow home price index portion
-    oldz = pd.read_sql('select * from zri;', engine) #remember to change
-    oldz.rename(columns = {'zrvi': 'zri'}, inplace = True)
-    oldz['d'] = 4
-    print(oldz.shape) #(218352, 5)
-
-    #stack the zillow tables
-    temp = pd.concat([oldz, zri_T], axis = 0) #(439747, 5)
-
-    #remove the duplicated, let's look at if there are still duplicated columns
-    temp.sort_values(by = ['zip', 'Home Type', 'Date', 'd'], inplace = True)
-    print(temp[['Date', 'Home Type', 'zip']].duplicated(keep = 'first').sum()) #218352
-
-    dups = temp[temp[['Date', 'Home Type', 'zip']].duplicated(keep = False)] #(436704, 5)
-    dups = dups.set_index(['Date', 'Home Type', 'zip', 'd']).unstack(level = -1).reset_index() #(218352, 5)
-    dups['p_diff'] = (dups.xs('zri', level=0, axis=1).max(axis = 1) - dups.xs('zri', level=0, axis=1).min(axis = 1)) / dups.xs('zri', level=0, axis=1).mean(axis = 1)
-    print(dups['p_diff'].max()) #the largest difference is 3%, which is kinda annoying but acceptable
-    print(dups.sort_values(by = 'p_diff', ascending = False).head())
-    
-
-    tempa = temp[(~temp[['Date', 'Home Type', 'zip']].duplicated(keep = 'first'))] #(221395, 5)
-    tempa.drop('d', axis = 1).to_sql('zillow_zri', engine, if_exists = 'replace', index = False) 
-
-    #Zillow Rental Index -
-    quandl_zri = {'ZRIAHMF': 'All Homes Multi Fam',
-                  'ZRIAH': 'All Homes',
-                  'ZRIMFRR': 'Multi Fam',
-                  'ZRISFRR': 'Single Fam'}
-
-    df_quandl_zri = pd.DataFrame()
-
-    for zip in df_state_city_zip_combined['state_city_zip_Name']:
-        for key in quandl_zri.keys():
-            link = 'ZILLOW/Z' + zip + '_' + key
-            try:
-                a = quandl.get(link)
-                a.index = [datetime.datetime(i.year, i.month, 1) for i in a.index]
-                a['zip'] = zip
-                a['Home Type'] = quandl_zri[key]
-                a.rename(columns = {'Value': 'zri'}, inplace = True)
-                df_quandl_zri = pd.concat([df_quandl_zri, a], axis = 0)
-            except Exception as e:            
-                if str(e) == '(Status 429) (Quandl Error QELx04) You have exceeded the API speed limit of 2000 calls per 10 minutes. Please slow down your requests.':
-                    try: #maybe i should use a while loop??
-                        print('error handling, try to pull with a different key \n')
-                        current_key = quandl_init(current_key)
-                        a = quandl.get(link)
-                        a.index = [datetime.datetime(i.year, i.month, 1) for i in a.index]
-                        a['zip'] = zip
-                        a['Home Type'] = quandl_zri[key]
-                        a.rename(columns = {'Value': 'zri'}, inplace = True)
-                        df_quandl_zri = pd.concat([df_quandl_zri, a], axis = 0)
-                    except Exception as o:
-                        print(link + 'second error' + str(o))
-                print(link + str(e))
-
-    print(df_quandl_zri.shape) #(204738, 3)
-    print(df_quandl_zri.duplicated(keep = 'first').sum()) #24120
-    df_quandl_zri.drop_duplicates(keep = 'first', inplace = True)
-    print(df_quandl_zri.shape) #(180618, 3)
-    print(df_quandl_zri.duplicated(keep = 'first').sum())
-    df_quandl_zri.index.name = 'Date'
-    df_quandl_zri.reset_index(inplace = True)
-    df_quandl_zri['d'] = 1
-
-    #quandl home price index portion
-    oldq = pd.read_sql('select * from quandl_zri;', engine)
-    oldq['d'] = 3
-    print(oldq.shape) #(178522, 5)
-
-    temp = pd.concat([oldq, df_quandl_zri], axis = 0) #(359140, 5)
-
-    #remove the duplicated, let's look at if there are still duplicated columns
-    temp.sort_values(by = ['zip', 'Home Type', 'Date', 'd'], inplace = True)
-    print(temp[['Date', 'Home Type', 'zip']].duplicated(keep = 'first').sum()) #168974
-
-    dups = temp[temp[['Date', 'Home Type', 'zip']].duplicated(keep = False)] #(337948, 5)
-    dups = dups.set_index(['Date', 'Home Type', 'zip', 'd']).unstack(level = -1).reset_index() #(168974, 5)
-    dups['p_diff'] = (dups.xs('zri', level=0, axis=1).max(axis = 1) - dups.xs('zri', level=0, axis=1).min(axis = 1)) / dups.xs('zri', level=0, axis=1).mean(axis = 1)
-    print(dups['p_diff'].max()) #the largest difference is 8%, which is kinda annoying. 3 observations
-    print(dups.sort_values(by = 'p_diff', ascending = False).head(10))
-
-    tempa = temp[(~temp[['Date', 'Home Type', 'zip']].duplicated(keep = 'first'))] #(62558, 5)
-    tempa.drop('d', axis = 1).to_sql('quandl_zri', engine, if_exists = 'replace', index = False)
-
-    #datasource comparision
-    #zillow home value index (ZHVI), we haven't changed this
-    #do some work to compare the two database
-    print(oldz[(oldz['zip'] == '10025') & (oldz['Home Type'] == 'All Homes')].shape) #(120, 4)
-    print(oldq[(oldq['zip'] == '10025') & (oldq['Home Type'] == 'All Homes')].shape) #(139, 4)
-    print(zhvi_T[(zhvi_T['zip'] == '10025') & (zhvi_T['Home Type'] == 'All Homes')].shape) #(120, 4)
-    print(df_quandl_zhvi[(df_quandl_zhvi['zip'] == '10025') & (df_quandl_zhvi['Home Type'] == 'All Homes')].shape) #(157, 4)
-
-    oldz[(oldz['zip'] == '10025') & (oldz['Home Type'] == 'All Homes')].to_csv('oz.csv', index = False)
-    oldq[(oldq['zip'] == '10025') & (oldq['Home Type'] == 'All Homes')].to_csv('oq.csv', index = False)
-    zhvi_T[(zhvi_T['zip'] == '10025') & (zhvi_T['Home Type'] == 'All Homes')].to_csv('z.csv', index = False)
-    df_quandl_zhvi[(df_quandl_zhvi['zip'] == '10025') & (df_quandl_zhvi['Home Type'] == 'All Homes')].to_csv('q.csv', index = False)
-
-    #combine these three tables: 1. oldz oldq, 2. zillow, 3. quandl
-    #stack the four tables
-    temp = pd.concat([oldz, oldq, zri_T, df_quandl_zri], axis = 0) #(798887, 5)
-
-    #remove the duplicated, let's look at if there are still duplicated columns
-    temp.sort_values(by = ['zip', 'Home Type', 'Date', 'd'], inplace = True)
-    print(temp[['Date', 'Home Type', 'zip']].duplicated(keep = 'first').sum()) #439385
-
-    dups = temp[temp[['Date', 'Home Type', 'zip']].duplicated(keep = False)] #(781041, 5)
-    dups = dups.set_index(['Date', 'Home Type', 'zip', 'd']).unstack(level = -1).reset_index() #(341656, 7)
-    dups['p_diff'] = (dups.xs('zri', level=0, axis=1).max(axis = 1) - dups.xs('zri', level=0, axis=1).min(axis = 1)) / dups.xs('zri', level=0, axis=1).mean(axis = 1)
-    print(dups['p_diff'].max()) #the largest difference is 7%, which is kinda annoying
-    print(dups.sort_values(by = 'p_diff', ascending = False).head(10))
-
-    tempa = temp[(~temp[['Date', 'Home Type', 'zip']].duplicated(keep = 'first'))].drop('d', axis = 1) #(359502, 4)
-
-
-    #proceed to fill in the data with interpolated values
-    temp1 = pd.DataFrame()
-
-    for zipcode in tempa['zip'].unique():
-        for home_type in tempa[tempa['zip'] == zipcode]['Home Type'].unique():
-            print(zipcode, home_type)
-            df = tempa[(tempa['zip'] == zipcode) & (tempa['Home Type'] == home_type)]
-            df.set_index('Date', inplace = True)
-            df = df.reindex(pd.date_range(start = df.index.min(), end = df.index.max(), freq = 'MS'), fill_value = np.nan)
-            mask = df['zip'].isnull()
-            df = df.interpolate(method = 'linear', axis = 0).ffill()
-            df['interp'] = mask
-            temp1 = pd.concat([temp1, df])
-
-    print(temp1.shape) #(369643, 4)
-
-    temp1.to_sql('combined_zri', engine, if_exists = 'replace', index = True)
-    pass
-
 
 def zipcode_update():
     old_zips, new_zips = zillow_zipcode_pull()
@@ -599,19 +433,21 @@ def get_zri(update = False):
     if update:
         df_state_city_zip_links = zillow_init()
 
-        zhvi, quandl_zhvi = zhvi(df_state_city_zip_links)
-        #zhvi as of 2018-04-30 (632589, 4)
-        #quandl_zhvi as of 2018-04-30 
+        zri, quandl_zri = zri(df_state_city_zip_links)
+        #zri as of 2018-04-30 (224976, 4)
+        #quandl_zri as of 2018-04-30(155080, 4)
 
+        bookmark = input('bookmark')
+        
         engine = create_engine('mysql+pymysql://a35931chi:Maggieyi66@localhost/realestate')
-        oldz = pd.read_sql('select * from zillow_zhvi;', engine)
-        #oldz as of 2018-04-30 (928125, 4)
+        oldz = pd.read_sql('select * from zillow_zri;', engine)
+        #oldz as of 2018-04-30 (221395, 4)
 
-        zhvi['source'] = 'Z'
-        quandl_zhvi['source'] = 'Q'
+        zri['source'] = 'Z'
+        quandl_zri['source'] = 'Q'
         oldz['source'] = 'S'
 
-        temp = pd.concat([oldz, zhvi, quandl_zhvi], axis = 0) #(2699730, 5)
+        temp = pd.concat([oldz, zri, quandl_zri], axis = 0) #(601451, 5)
         temp = temp.set_index(['Date', 'Home Type', 'zip', 'source']).unstack(level = -1).reset_index()
 
         temp['p_diff'] = (temp.xs('zri', level = 0, axis = 1).max(axis = 1) - temp.xs('zri', level = 0, axis = 1).min(axis = 1)) / temp.xs('zri', level = 0, axis = 1).mean(axis = 1)
@@ -621,7 +457,7 @@ def get_zri(update = False):
         #2. take SQL's answer, if Zillow is missing and SQL is not missing
         #3. take Quandl's answer, if both Zillow and SQL is missing
         
-        def condense_zhvi(df):
+        def condense_zri(df):
             if ~pd.isnull(df['Z']):
                 return df['Z']
             elif ~pd.isnull(df['S']):
@@ -629,12 +465,12 @@ def get_zri(update = False):
             else:
                 return df['Q']
 
-        temp['zhvi_final'] = temp['zhvi'].apply(condense_zhvi, axis = 1)
+        temp['zri_final'] = temp['zri'].apply(condense_zri, axis = 1)
 
         #just checking
-        print(temp[temp.xs('Z', level= 1, axis=1).isnull()['zhvi']].head())
-        print(temp[(temp.xs('Z', level= 1, axis=1).isnull()['zhvi']) & temp.xs('S', level= 1, axis=1).isnull()['zhvi']].head())
-        print(temp[temp.xs('Q', level= 1, axis=1).isnull()['zhvi']].head())
+        print(temp[temp.xs('Z', level= 1, axis=1).isnull()['zri']].head())
+        print(temp[(temp.xs('Z', level= 1, axis=1).isnull()['zri']) & temp.xs('S', level= 1, axis=1).isnull()['zri']].head())
+        print(temp[temp.xs('Q', level= 1, axis=1).isnull()['zri']].head())
         
         tempa = temp[['Date', 'Home Type', 'zip', 'zri_final']].rename(columns = {'zri_final': 'zri'})
         bookmark = input('Are you sure you want to update SQL table? (Y/N)')
@@ -665,71 +501,10 @@ def get_zri(update = False):
     
             
 if __name__ == '__main__':
-    df_state_city_zip_links = zillow_init()
-
-    zri, quandl_zri = zri(df_state_city_zip_links)
-    #zhvi as of 2018-04-30 (632589, 4)
-    #quandl_zhvi as of 2018-04-30 
-
-    bookmark = input('bookmark')
-    
-    engine = create_engine('mysql+pymysql://a35931chi:Maggieyi66@localhost/realestate')
-    oldz = pd.read_sql('select * from zillow_zri;', engine)
-    #oldz as of 2018-04-30 (928125, 4)
-
-    zri['source'] = 'Z'
-    quandl_zri['source'] = 'Q'
-    oldz['source'] = 'S'
-
-    temp = pd.concat([oldz, zri, quandl_zri], axis = 0) #(2699730, 5)
-    temp = temp.set_index(['Date', 'Home Type', 'zip', 'source']).unstack(level = -1).reset_index()
-
-    temp['p_diff'] = (temp.xs('zri', level = 0, axis = 1).max(axis = 1) - temp.xs('zri', level = 0, axis = 1).min(axis = 1)) / temp.xs('zri', level = 0, axis = 1).mean(axis = 1)
-    
-    #my rule:
-    #1. take Zillow's answer, if Zillow not missing
-    #2. take SQL's answer, if Zillow is missing and SQL is not missing
-    #3. take Quandl's answer, if both Zillow and SQL is missing
-    
-    def condense_zri(df):
-        if ~pd.isnull(df['Z']):
-            return df['Z']
-        elif ~pd.isnull(df['S']):
-            return df['S']
-        else:
-            return df['Q']
-
-    temp['zri_final'] = temp['zri'].apply(condense_zri, axis = 1)
-
-    #just checking
-    print(temp[temp.xs('Z', level= 1, axis=1).isnull()['zhvi']].head())
-    print(temp[(temp.xs('Z', level= 1, axis=1).isnull()['zhvi']) & temp.xs('S', level= 1, axis=1).isnull()['zhvi']].head())
-    print(temp[temp.xs('Q', level= 1, axis=1).isnull()['zhvi']].head())
-    
-    tempa = temp[['Date', 'Home Type', 'zip', 'zri_final']].rename(columns = {'zri_final': 'zri'})
-    bookmark = input('Are you sure you want to update SQL table? (Y/N)')
-    if bookmark == 'Y':
-        engine = create_engine('mysql+pymysql://a35931chi:Maggieyi66@localhost/realestate')
-        tempa.to_sql('zillow_zri', engine, if_exists = 'replace', index = False)
-        print('SQL table Update Completed')
-    else:
-        print('SQL table was not updated')
-else:
-    tempa = oldz
-    
-#proceed to fill in the data with interpolated values
-temp1 = pd.DataFrame()
-
-for zipcode in tempa['zip'].unique():
-    for home_type in tempa[tempa['zip'] == zipcode]['Home Type'].unique():
-        #print(zipcode, home_type)
-        df = tempa[(tempa['zip'] == zipcode) & (tempa['Home Type'] == home_type)]
-        df.set_index('Date', inplace = True)
-        df = df.reindex(pd.date_range(start = df.index.min(), end = df.index.max(), freq = 'MS'), fill_value = np.nan)
-        mask = df['zip'].isnull()
-        df = df.interpolate(method = 'linear', axis = 0).ffill()
-        df['interp'] = mask
-        temp1 = pd.concat([temp1, df])
+    #the only three things you should call:
+    #1. zipcode_update()
+    #2. get_zhvi(if you want to pull new info, then True)
+    #3. get_zri(if you want to pull new info, then True)
 
 
 
